c4853789fc4005d22a16437726e87be6d817afc4
Chaolin Han <chaosrinkhan@gmail.com>
2020-05-17 22:38:33 +0100

    New prior range adjasted

--- pyABC_study/inferBack.py
+++ pyABC_study/inferBack.py
@@ -8,6 +8,7 @@ import copy
 
 from pyABC_study.ODE import ODESolver, euclidean_distance, normalise_data
 
+ROOT_DIR = os.path.abspath(os.curdir)
 db_path = ("sqlite:///" +
            os.path.join(tempfile.gettempdir(), "test.db"))
 
@@ -23,25 +24,25 @@ solver = ODESolver()
 expData = solver.ode_model(paraInit)
 normalise_data(expData)
 
-print("Target data\n")
+print("Target data")
 print(expData)
 
 
 # Define prior distribution of parameters
 
 paraPrior = pyabc.Distribution(
-    lambdaN=pyabc.RV("uniform", 8, 15),
-    kNB=pyabc.RV("uniform", -3, 2),
-    muN=pyabc.RV("uniform", -5, 2),
-    vNM=pyabc.RV("uniform", -7, 5),
+    lambdaN=pyabc.RV("uniform", 0, 15),
+    kNB=pyabc.RV("uniform", -10, 2),
+    muN=pyabc.RV("uniform", -10, 2),
+    vNM=pyabc.RV("uniform", -15, 2),
     lambdaM=pyabc.RV("uniform", -3, 7),
-    kMB=pyabc.RV("uniform", -5, 3),
+    kMB=pyabc.RV("uniform", -10, 2),
     muM=pyabc.RV("uniform", -3, 4),
-    sBN=pyabc.RV("uniform", -3, 2),
-    iBM=pyabc.RV("uniform", -3, 2),
+    sBN=pyabc.RV("uniform", -10, 2),
+    iBM=pyabc.RV("uniform", -7, 2),
     muB=pyabc.RV("uniform", 0, 7),
-    sAM=pyabc.RV("uniform", 3, 18),
-    muA=pyabc.RV("uniform", 10, 60)
+    sAM=pyabc.RV("uniform", -5, 20),
+    muA=pyabc.RV("uniform", 0, 60)
 )
 
 # Define ABC-SMC model
@@ -52,7 +53,7 @@ abc = pyabc.ABCSMC(models=solver.ode_model,
                    parameter_priors=paraPrior,
                    distance_function=euclidean_distance,
                    #distance_function=distance_adaptive,
-                   population_size=1000,
+                   population_size=500,
                    eps=pyabc.MedianEpsilon(50, median_multiplier=1)
                    )
 
@@ -77,25 +78,27 @@ plt.show()
 # Make a gif
 
 limits = dict(
-    lambdaN=(8, 15),
-    kNB=(-3, 2),
-    muN=(-5, 2),
-    vNM=(-7, 5),
+    lambdaN=(0, 15),
+    kNB=(-10, 2),
+    muN=(-10, 2),
+    vNM=(-15, 2),
     lambdaM=(-3, 7),
-    kMB=(-5, 3),
+    kMB=(-10, 2),
     muM=(-3, 4),
-    sBN=(-3, 2),
-    iBM=(-3, 2),
+    sBN=(-10, 2),
+    iBM=(-7, 2),
     muB=(0, 7),
-    sAM=(3, 18),
-    muA=(10, 60)
+    sAM=(-5, 20),
+    muA=(0, 60)
               )
 
 df, w = history.get_distribution(t=24)
 pyabc.visualization.plot_kde_matrix(df, w, limits=limits)
 
+# TODO make file name dynamic
+
 for tt in range(1,24):
-    filename = r"/home/yuan/wdnmd/MSc_Project/pyABC_study/p1000e50t25/"+str(tt)+".png"
+    filename = ROOT_DIR+"/pyABC_study/plot/p500e50t25/"+str(tt)+".png"
     df, w = history.get_distribution(t=tt)
-    pyabc.visualization.plot_kde_matrix(df, w)
+    pyabc.visualization.plot_kde_matrix(df, w, limits=limits)
     plt.savefig(filename)
